4a3abc7cad617ca4f929eab30280a35e6c99be7d
kinggodchan <kyc010264@gmail.com>
2025-03-08 01:58:34 +0900

    feat: 경기 일정 생성 기능 구현을 위한 작업

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,48 +1,48 @@
-import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { UserRole } from "./user-role.enum";
+import { Team } from "src/teams/entities/team.entity";  // 팀 엔티티 추가
 import { Article } from "src/articles/entities/article.entity";
 import { Comment } from "src/comment/entities/comment.entity";
 import { TradePost } from "src/board/entities/trade-post.entity";
-import { Report } from "src/report/entities/report.entity";
-  
-  @Entity()
-  export class User {
-    @PrimaryGeneratedColumn()
-    user_id: number;
-  
-    @Column()
-    username: string;
-  
-    @Column()
-    password: string;
-  
-    @Column({ unique: true }) 
-    email: string;
-  
-    @Column({ length: 20, nullable: false })
-    phone_number: string;
-  
-    @Column({ length: 255, nullable: true })
-    location?: string;
-    
-    @Column()
-    role: UserRole;
-  
-    @OneToMany(() => Article, (article) => article.author, { eager: false })
-    articles: Article[];
-  
-    // Comment와의 관계 설정 (작성한 댓글들)
-    @OneToMany(() => Comment, (comment) => comment.author, { cascade: true })
-    comments: Comment[];
-  
-    // TradePost와의 관계 설정 (작성한 거래 게시글)
-    @OneToMany(() => TradePost, (tradePost) => tradePost.author, { cascade: true })
-    tradePosts: TradePost[];
-  
-    @CreateDateColumn({ type: 'timestamp' })
-    createAt: Date;
-    
-    @UpdateDateColumn({ type: 'timestamp' })
-    updateAt: Date;
-  }
-  
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true }) 
+  email: string;
+
+  @Column({ length: 20, nullable: false })
+  phone_number: string;
+
+  @Column({ length: 255, nullable: true })
+  location?: string;
+
+  @Column()
+  role: UserRole;
+
+  @ManyToMany(() => Team, (team) => team.members, { eager: true })
+  teams: Team[];  // 유저가 여러 팀에 속할 수 있음
+
+  @OneToMany(() => Article, (article) => article.author, { eager: false })
+  articles: Article[];
+
+  @OneToMany(() => Comment, (comment) => comment.author, { cascade: true })
+  comments: Comment[];
+
+  @OneToMany(() => TradePost, (tradePost) => tradePost.author, { cascade: true })
+  tradePosts: TradePost[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updateAt: Date;
+}
