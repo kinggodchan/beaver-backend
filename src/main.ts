c17fcb13740ed46117105b6e88a607e6158b5e82
kinggodchan <kyc010264@gmail.com>
2025-03-15 16:13:49 +0900

    feat: react와 연동시키기 위한 준비

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as multer from 'multer';
 
 dotenv.config();
 
@@ -9,19 +10,30 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 파일 업로드를 위한 multer 설정
-  // app.use(
-  //   multer({ dest: './uploads' }).single('image'),  // 'image'는 form-data의 키
-  // );
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads'); // 업로드할 경로
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname); // 파일 이름 설정
+    },
+  });
 
+  const upload = multer({ storage });
+
+  // 파일 업로드 라우터 설정 (예시)
+  app.use('/upload', upload.single('image')); // 'image'는 form-data의 키
+
+  // CORS 설정
   app.enableCors({
-    origin:'http://localhost:4200',
-    credential: true,
-    exposedHeader: ['Authorization']
-  })
-  
-  
+    origin: 'http://localhost:4200', // React 앱의 주소
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+  });
+
   const port = process.env.PORT ?? 3000; // 기본값 설정
   await app.listen(port);
-  Logger.log(`Application Running on Port : ${process.env.PORT}`)
+  Logger.log(`Application Running on Port: ${port}`); // 환경변수에서 포트 가져오기
 }
+
 bootstrap();
