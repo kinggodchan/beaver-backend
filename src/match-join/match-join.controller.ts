a51d10de270668e2a6ba218a01208ac433985888
seranomi <2bumchan@naver.com>
2025-03-30 15:47:47 +0900

    feat: 경기 신청 및 조회 기능 구현

--- src/match-join/match-join.controller.ts
+++ src/match-join/match-join.controller.ts
@@ -1,20 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+  HttpStatus,
+} from '@nestjs/common';
 import { MatchJoinService } from './match-join.service';
 import { CreateMatchJoinDto } from './dto/create-match-join.dto';
 import { UpdateMatchJoinDto } from './dto/update-match-join.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/custom-guards-decorators/custom-role.guard';
+import { Roles } from 'src/auth/custom-guards-decorators/roles.decorator';
+import { UserRole } from 'src/users/entities/user-role.enum';
+import { GetUser } from 'src/auth/custom-guards-decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { ApiResponseDto } from 'src/common/api-response-dto/api-response.dto';
+import { MatchJoin } from './entities/match-join.entity';
+import { MatchJoinResponseDto } from './dto/match-join-response.dto';
 
-@Controller('match-join')
+@Controller('api/matches')
 export class MatchJoinController {
   constructor(private readonly matchJoinService: MatchJoinService) {}
 
-  @Post()
-  create(@Body() createMatchJoinDto: CreateMatchJoinDto) {
-    return this.matchJoinService.create(createMatchJoinDto);
+  @Post('/:matchId/join')
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles(UserRole.USER)
+  async joinMatch(
+    @Param('matchId', ParseIntPipe) matchId: number,
+    @GetUser() user: User,
+  ): Promise<ApiResponseDto<void>> {
+    await this.matchJoinService.requestJoin(matchId, user);
+    return new ApiResponseDto(true, HttpStatus.OK, '경기 신청 완료');
   }
 
-  @Get()
-  findAll() {
-    return this.matchJoinService.findAll();
+  @Get('/:matchId/join/all')
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles(UserRole.USER)
+  async getJoinRequests(
+    @Param('matchId', ParseIntPipe) matchId: number,
+    @GetUser() user: User,
+  ): Promise<ApiResponseDto<MatchJoinResponseDto[]>> {
+    const joins = await this.matchJoinService.getJoinRequests(matchId, user);
+    const responseDtos = joins.map((join) => new MatchJoinResponseDto(join));
+    return new ApiResponseDto(
+      true,
+      HttpStatus.OK,
+      '신청 목록 조회 성공',
+      responseDtos,
+    );
   }
 
   @Get(':id')
@@ -23,7 +61,10 @@ export class MatchJoinController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMatchJoinDto: UpdateMatchJoinDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateMatchJoinDto: UpdateMatchJoinDto,
+  ) {
     return this.matchJoinService.update(+id, updateMatchJoinDto);
   }
 
--- src/match-join/match-join.module.ts
+++ src/match-join/match-join.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MatchJoinService } from './match-join.service';
 import { MatchJoinController } from './match-join.controller';
+import { Match } from 'src/match/entities/match.entity';
+import { Team } from 'src/teams/entities/team.entity';
+import { MatchJoin } from './entities/match-join.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([MatchJoin, Match, Team]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [MatchJoinController],
   providers: [MatchJoinService],
 })
