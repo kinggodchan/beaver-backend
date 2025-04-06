dd71be566c118f51d36e99feecb4198c4947640c
seranomi <2bumchan@naver.com>
2025-04-06 16:08:11 +0900

    refactor: MatchResponseDto의 팀 정보를 Dto로 감싸도록 변경

--- src/match/dto/match-response.dto.ts
+++ src/match/dto/match-response.dto.ts
@@ -1,13 +1,12 @@
 // dto/match-response.dto.ts
 import { MatchResultResponseDto } from 'src/match-result/dto/match-result-response.dto';
 import { Match } from '../entities/match.entity';
+import { TeamResponseDto } from 'src/teams/dto/team-response.dto';
 
 export class MatchResponseDto {
   match_id: number;
-  host_team_id: number;
-  host_team_name: string;
-  opponent_team_id?: number;
-  opponent_team_name?: string;
+  host_team: TeamResponseDto;
+  opponent_team?: TeamResponseDto;
   match_date: Date;
   location: string;
   status: string;
@@ -15,17 +14,16 @@ export class MatchResponseDto {
 
   constructor(match: Match) {
     this.match_id = match.match_id;
-    this.host_team_id = match.host_team.team_id;
-    this.host_team_name = match.host_team.team_name;
-    this.opponent_team_id = match.opponent_team?.team_id;
-    this.opponent_team_name = match.opponent_team?.team_name;
+    this.host_team = new TeamResponseDto(match.host_team);
     this.match_date = match.match_date;
     this.location = match.location;
     this.status = match.status;
 
-    // ✅ result 있을 때만 요약 형태로 담기
     if (match.result) {
       this.result = new MatchResultResponseDto(match.result);
+    }
+    if (match.opponent_team) {
+      this.opponent_team = new TeamResponseDto(match.opponent_team);
     }
   }
 }
